a174214c19551da8744be4b79d17edb0868abcb2
Anthony Ku <anthony.yc.ku@gmail.com>
2021-03-30 16:06:48 -0700

    added serverside email validation to controller/customer.js

--- app/controllers/customers/customer.js
+++ app/controllers/customers/customer.js
@@ -1,11 +1,25 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class CustomersCustomerController extends Controller {
+    @tracked validEmail = false;
+
     @action
     async saveCustomer(event) {
         event.preventDefault();
-        await this.model.save();
-        this.transitionToRoute('customers.index');
+
+        if (this.validate(this.model.emailAddress)) {
+            await this.model.save();
+            this.transitionToRoute('customers.index');
+        } else {
+            console.log("need valid email")
+        }
+    }
+
+    validate = (email) => {
+        const regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        email.match(regex) ? this.validEmail = true : this.validEmail = false;
+        return email.match(regex);
     }
 }
