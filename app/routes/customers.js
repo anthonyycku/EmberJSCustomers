e43db2513d6c8b32114ca0c37d00f05da67f137c
Anthony Ku <anthony.yc.ku@gmail.com>
2021-03-30 12:26:06 -0700

    fixed email value in edit page

--- app/routes/customers.js
+++ app/routes/customers.js
@@ -1,9 +1,4 @@
 import Route from '@ember/routing/route';
 
 export default class CustomersRoute extends Route {
-    async model() {
-        return {
-            firstName: "hello"
-        }
-    }
 }
