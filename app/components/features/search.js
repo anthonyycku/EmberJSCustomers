7265aa86832b962c11b5621141fe0dd0f10edf55
Anthony Ku <anthony.yc.ku@gmail.com>
2021-03-30 19:47:02 -0700

    updated search to track user input

--- app/components/features/search.js
+++ app/components/features/search.js
@@ -1,4 +1,18 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class SearchComponent extends Component {
+    @tracked current = "";
+
+    @action
+    update(event) {
+        this.current = event.target.value;
+    }
+
+    @action
+    search(event) {
+        event.preventDefault();
+        console.log(this.current);
+    }
 }
