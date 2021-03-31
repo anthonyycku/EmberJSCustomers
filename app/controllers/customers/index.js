d6441f3708acb3620c4c80f0c0816c33bab7f7be
Anthony Ku <anthony.yc.ku@gmail.com>
2021-03-31 10:07:11 -0700

    refactored sorting code in index.js

--- app/controllers/customers/index.js
+++ app/controllers/customers/index.js
@@ -4,27 +4,24 @@ import { sort } from '@ember/object/computed';
 import { tracked } from '@glimmer/tracking';
 
 export default class CustomersIndexController extends Controller {
-  @tracked sortProperty = 'lastNameAsc';
+  @tracked sortProperty = 'lastName';
+  @tracked sortID = "lastNameAsc";
 
   @sort('model', 'customersSortProps')
   sortedCustomers;
 
-
   get customersSortProps() {
     switch (this.sortProperty) {
-      case "budgetAsc":
-        return ["budget"];
-      case "budgetDesc":
-        return ["budget:desc"];
-      case "lastNameAsc":
-        return ["lastName"];
-      case "lastNameDesc":
-        return ["lastName:desc"];
+      case "budget":
+        return this.sortID === "budgetAsc" ? ["budget"] : ["budget:desc"];
+      case "lastName":
+        return this.sortID === "lastNameAsc" ? ["lastName"] : ["lastName:desc"];
     }
   }
 
   @action
   updateSortProperty(event) {
     this.sortProperty = event.target.value
+    this.sortID = event.target[event.target.selectedIndex].id
   }
 }
